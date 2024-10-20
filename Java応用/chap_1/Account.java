9cd3f3966f5403f3e7fe401a26e7e3c2760f8a3a
yanasekota <09041150239@docomo.ne.jp>
2024-10-20 22:02:15 +0900

    オーバーライド git commit -m オーバーライド”

--- Java応用/chap_1/Account.java
+++ Java応用/chap_1/Account.java
@@ -1,8 +1,18 @@
 package chap_1;
 
-public class Account {
+public class Account implements Comparable<Account>{
 	String accountNo;
-	
+	int number;
+	int zandaka;
+	public int compareTo (Account obj) {
+		if(this.number < obj.number) {
+			return -1;
+		}
+		if(this.number > obj.number) {
+			return 1;
+		}
+		return 0;
+	}
 	public boolean equals(Object o) {
 		if(o == this) return true;
 		if(o == null) return false;
---
+++ Java応用/chap_1/Hero.java
@@ -0,0 +1,10 @@
+package chap_1;
+
+public class Hero {
+	public String name;
+	public boolean equals(Object o){
+		if (o == this) return true;
+		return true;
+	}
+
+}
