3a04dd768ecd8293f344596fe264fd2b517c3e44
yanasekota <09041150239@docomo.ne.jp>
2024-10-21 22:19:51 +0900

    クローン

--- Java応用/chap_1/Hero.java
+++ Java応用/chap_1/Hero.java
@@ -1,10 +1,19 @@
 package chap_1;
 
-public class Hero {
+public class Hero implements Cloneable{
 	public String name;
+	int hp;
+	String sword;
 	public boolean equals(Object o){
 		if (o == this) return true;
 		return true;
 	}
-
+	
+	public Hero clone() {
+		Hero result = new Hero();
+		result.name = this.name;
+		result.hp = this.hp;
+		result.sword = this.sword;
+		return result;
+	}
 }
